1fe2b9b1cc82cc5a769dc09691ee307e270efb38
Marcel Müller <pm123@maazl.de>
2007-02-07 11:56:49 +0000

    Fix: C++ guard

--- src/utils/snprintf.h
+++ src/utils/snprintf.h
@@ -1,6 +1,10 @@
 #ifndef PORTABLE_SNPRINTF_H
 #define PORTABLE_SNPRINTF_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdarg.h>
 
 #if defined(__GNUC__) || defined(__WATCOMC__)
@@ -9,4 +13,8 @@
 extern int snprintf ( char*, size_t, const char*, /*args*/ ... );
 extern int vsnprintf( char*, size_t, const char*, va_list );
 #endif
+#endif
+
+#ifdef __cplusplus
+}
 #endif
