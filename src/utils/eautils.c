53b1b4ee3da3f3fdba8ce3ec2e25a149869aa91b
Marcel Müller <pm123@maazl.de>
2009-02-04 22:51:16 +0000

    EA handling

--- src/utils/eautils.c
+++ src/utils/eautils.c
@@ -38,12 +38,9 @@
 /* Copies extended attributes from one file or directory to another.
  * Attributes are added to a target file or replaced.
  */
-
 APIRET
 eacopy( const char* source, const char* target )
 {
-  ULONG  action;
-  HFILE  hsource  = NULLHANDLE;
   APIRET rc       = 0;
   EAOP2  ea_op    = { 0 };
   DENA2* ea_names = NULL;
@@ -56,23 +53,6 @@ eacopy( const char* source, const char* target )
   FEA2LIST* ea_value_list = NULL;
 
   FILESTATUS4 fs;
-
-  rc = DosQueryPathInfo((PSZ)source, FIL_STANDARD, &fs, sizeof( fs ));
-
-  if( rc != NO_ERROR ) {
-    DEBUGLOG(( "eacopy: Fail at DosQueryPathInfo of file %s, rc= %08X\n", source, rc ));
-    return rc;
-  }
-
-  if(!( fs.attrFile & FILE_DIRECTORY )) {
-    rc = DosOpen((PSZ)source, &hsource, &action, 0, 0, OPEN_ACTION_OPEN_IF_EXISTS,
-                  OPEN_ACCESS_READONLY | OPEN_SHARE_DENYNONE | OPEN_FLAGS_FAIL_ON_ERROR, NULL );
-
-    if( rc != NO_ERROR ) {
-      DEBUGLOG(( "eacopy: Fail at DosOpen of file %s, rc= %08X\n", source, rc ));
-      return rc;
-    }
-  }
 
   for(;;) {
     rc = DosQueryPathInfo((PSZ)source, FIL_QUERYEASIZE, &fs, sizeof( fs ));
@@ -162,12 +142,118 @@ eacopy( const char* source, const char* target )
   free( ea_names );
   free( ea_names_list );
 
-  if( hsource != NULLHANDLE ) {
-    DosClose( hsource );
-  }
-
   return rc;
 }
+
+/* Copies extended attributes from one file or directory to another.
+ * Attributes are added to a target file or replaced.
+ * The source file must be opened with read access and deny write access.
+ */
+/*APIRET eacopy2( HFILE source, const char* target )
+{
+  APIRET rc       = 0;
+  EAOP2  ea_op    = { 0 };
+  DENA2* ea_names = NULL;
+  DENA2* ea_found = NULL;
+  ULONG  ea_count = -1;
+  ULONG  ea_size;
+  GEA2*  p;
+
+  GEA2LIST* ea_names_list = NULL;
+  FEA2LIST* ea_value_list = NULL;
+
+  FILESTATUS4 fs;
+
+  for(;;) {
+    rc = DosQueryFileInfo(source, FIL_QUERYEASIZE, &fs, sizeof( fs ));
+
+    if( rc != NO_ERROR ) {
+      DEBUGLOG(( "eacopy: Fail at DosQueryPathInfo of, rc= %08X\n", rc ));
+      break;
+    }
+
+    DEBUGLOG2(( "eacopy: EAs size is %d bytes.\n", fs.cbList ));
+
+    // The buffer size is less than or equal to twice the size
+    // of the file's entire EA set on disk.
+    ea_size  = fs.cbList * 2;
+    ea_names = malloc( ea_size );
+    ea_found = ea_names;
+
+    if( !ea_names ) {
+      DEBUGLOG(( "eacopy: Not enough memory.\n" ));
+      rc = ERROR_NOT_ENOUGH_MEMORY;
+      break;
+    }
+
+    rc = DosEnumAttribute( ENUMEA_REFTYPE_FHANDLE, (PVOID)source, 1, ea_names, ea_size,
+                           &ea_count, ENUMEA_LEVEL_NO_VALUE );
+
+    if( rc != NO_ERROR ) {
+      DEBUGLOG(( "eacopy: Fail at DosEnumAttribute, rc= %08X\n", rc ));
+      break;
+    }
+
+    DEBUGLOG2(( "eacopy: Have %d EAs.\n", ea_count ));
+
+    if( !ea_count ) {
+      break;
+    }
+
+    ea_names_list = calloc( 1, ea_size );
+
+    if( !ea_names_list ) {
+      DEBUGLOG(( "eacopy: Not enough memory.\n" ));
+      rc = ERROR_NOT_ENOUGH_MEMORY;
+      break;
+    }
+
+    p = ea_names_list->list;
+
+    while( ea_count-- )
+    {
+      int size;
+      DEBUGLOG2(( "eacopy: %s\n", ea_found->szName ));
+
+      p->cbName = ea_found->cbName;
+      strcpy( p->szName, ea_found->szName );
+      size = sizeof( GEA2 ) + p->cbName;
+      // The GEA2 data structures must be aligned on a doubleword boundary.
+      size = (( size - 1 ) / sizeof( ULONG ) + 1 ) * sizeof( ULONG );
+      p->oNextEntryOffset = ea_count ? size : 0;
+      p = (GEA2*)((char*)p + size );
+      ea_found = (DENA2*)((char*)ea_found + ea_found->oNextEntryOffset );
+    }
+
+    ea_value_list = (FEA2LIST*)ea_names;
+    ea_names_list->cbList = (char*)p - (char*)ea_names_list;
+    ea_value_list->cbList = ea_size;
+    ea_op.fpGEA2List = ea_names_list;
+    ea_op.fpFEA2List = ea_value_list;
+
+    rc = DosQueryFileInfo(source, FIL_QUERYEASFROMLIST, &ea_op, sizeof( ea_op ));
+
+    if( rc != NO_ERROR ) {
+      DEBUGLOG(( "eacopy: Fail at DosQueryPathInfo, rc= %08X\n", rc ));
+      break;
+    }
+
+    rc = DosSetPathInfo((PSZ)target, FIL_QUERYEASIZE, &ea_op, sizeof( ea_op ), 0 );
+
+    if( rc != NO_ERROR ) {
+      DEBUGLOG(( "eacopy: Fail at DosSetPathInfo of file %s, rc= %08X\n", target, rc ));
+      break;
+    }
+
+    rc = NO_ERROR;
+    break;
+  }
+
+  free( ea_names );
+  free( ea_names_list );
+
+  return rc;
+}*/
 
 APIRET eaget( const char* file, const char* eaname, char** eadata, size_t* easize )
 {
