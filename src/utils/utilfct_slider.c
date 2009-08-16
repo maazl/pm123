e52c77bd3ccf1f56a36c2446c79acd712696249e
Marcel Müller <pm123@maazl.de>
2009-08-16 22:21:49 +0000

    missing file

---
+++ src/utils/utilfct_slider.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 1997-2003 Samuel Audet <guardia@step.polymtl.ca>
+ *                     Taneli Lepp„ <rosmo@sektori.com> *
+ * Copyright 2004-2006 Dmitry A.Steklenev <glass@ptv.ru>
+ * Copyright 2006-2009 Marcel Mueller
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    3. The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define  INCL_PM
+
+#include "utilfct.h"
+#include <os2.h>
+
+#include "debuglog.h"
+
+BOOL cs_init( HWND hwnd, USHORT id, LONG low, LONG high, LONG inc, LONG tick, LONG value )
+{ HWND ctrl = WinWindowFromID( hwnd, id );
+  return ctrl != NULLHANDLE
+    && WinSendMsg( ctrl, CSM_SETRANGE,     MPFROMLONG(low),   MPFROMLONG(high))
+    && WinSendMsg( ctrl, CSM_SETINCREMENT, MPFROMLONG(inc),   MPFROMLONG(tick))
+    && WinSendMsg( ctrl, CSM_SETVALUE,     MPFROMLONG(value), NULL);
+}
+
+
