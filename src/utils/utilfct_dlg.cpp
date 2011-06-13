6dfd75bdcdb03776885c42ee5c46e822d3642024
Marcel Müller <pm123@maazl.de>
2011-06-13 09:19:52 +0000

    Automatic dialog resizing, missing file

---
+++ src/utils/utilfct_dlg.cpp
@@ -0,0 +1,225 @@
+/*
+ * Copyright 1997-2003 Samuel Audet <guardia@step.polymtl.ca>
+ *                     Taneli Lepp� <rosmo@sektori.com> *
+ * Copyright 2004-2006 Dmitry A.Steklenev <glass@ptv.ru>
+ * Copyright 2006-2011 Marcel Mueller
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    3. The name of the author may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define  INCL_PM
+#include "utilfct.h"
+#include <os2.h>
+#include <string.h>
+
+
+#include "debuglog.h"
+
+
+/** Structure with parameters of \c WM_SIZE. */
+typedef struct
+{ SHORT cxold;
+  SHORT cyold;
+  SHORT cxnew;
+  SHORT cynew;
+} SWRS;
+
+typedef struct
+{ PPResizeInfo   rs;
+  PPResizeConstr cs;
+} PPResizeParams;
+
+
+/** Caluate the resize delta from old and new size.
+ * @param oldsize Size of the owner before resizing
+ * @param newsize New size of the owner
+ * @param factor  Rational resize factor.
+ * @return        Size change to be applied to the child control.
+ */
+INLINE int calc_delta(SHORT oldsize, SHORT& newsize, PPResizeFactor factor)
+{ return newsize*factor.Numerator/factor.Denominator - oldsize*factor.Numerator/factor.Denominator;
+}
+/** Caluate the resize delta from old and new size with min size constraint.
+ * @param oldsize Size of the owner before resizing
+ * @param delta   Maximum allowed shrinking. This should usually be negative,
+ *                but it might be positive if the minimum size constraint is already violated.
+ *                In this case the current size is kept.
+ * @param factor  Rational resize factor.
+ * @return        New size of the owner.
+ */
+INLINE int calc_newsize(SHORT oldsize, int& delta, PPResizeFactor factor)
+{ if (delta > 0)
+    delta = 0;
+  int new_scaled = oldsize*factor.Numerator/factor.Denominator + delta;
+  return new_scaled*factor.Denominator/factor.Numerator;
+}
+
+/**
+ * Applies a size change to a child control using the resize info presentation parameters.
+ * @param pos    Child window position, is modified in place to meet the new size.
+ * @param rsinfo Resizing information from presentation parameter \c PPU_RESIZEINFO.
+ * @param resize Requested size change. The cxnew and cynew fields might be adjusted
+ *               if the resizing would cause a minimum size constraint in the \a rsinfo
+ *               to be violated otherwise.
+ * @remarks Note that the fact that the resize information may be changed causes
+ * the resizing to get inconsistent if some child controls already have been resized
+ * without checking the constrains of all siblings. Because of that you must operate
+ * with two passes. In the first the resize should be simulated and the constraints should
+ * be applied, in the second pass the resize actually takes place. The first pass
+ * should be done in \c WM_ADJUSTWINDOWPOS processing, while the second pass should be done
+ * in \c WM_WINDOWPOSCHANGED processing.
+ */
+static void apply_resize(SWP& pos, const PPResizeParams& rsinfo, SWRS& resize)
+{ // x resize
+  int delta = calc_delta(resize.cxold, resize.cxnew, rsinfo.rs.cx_resize);
+  if (delta < 0 && pos.cx + delta < rsinfo.cs.cx_min)
+    // Violated min constraint => adjust cxnew
+    resize.cxnew = calc_newsize(resize.cxold, delta = rsinfo.cs.cx_min - pos.cx, rsinfo.rs.cx_resize);
+  pos.cx += delta;
+  pos.x += calc_delta(resize.cxold, resize.cxnew, rsinfo.rs.x_resize);
+  // y resize
+  delta = calc_delta(resize.cyold, resize.cynew, rsinfo.rs.cy_resize);
+  if (delta < 0 && pos.cy + delta < rsinfo.cs.cy_min)
+    // Violated min constraint => adjust cynew
+    resize.cynew = calc_newsize(resize.cyold, delta = rsinfo.cs.cy_min - pos.cy, rsinfo.rs.cy_resize);
+  pos.cy += delta;
+  pos.y += calc_delta(resize.cyold, resize.cynew, rsinfo.rs.y_resize);
+  // identify move operation
+  pos.fl |= SWP_MOVE|SWP_SIZE;
+}
+
+void dlg_adjust_resize(HWND hwnd, SWP* pswp)
+{ DEBUGLOG(("dlg_adjust_resize(%p, {%x, %i,%i, %i,%i})\n", hwnd,
+    pswp->fl, pswp->cx, pswp->cy, pswp->x, pswp->y));
+
+  // query current size
+  SWP cursize;
+  PMRASSERT(WinQueryWindowPos(hwnd, &cursize));
+  if (cursize.cx == pswp->cx && cursize.cy == pswp->cy)
+    return;
+  SWRS sizechg = { cursize.cx, cursize.cy, pswp->cx, pswp->cy };
+
+  // Apply size constraint of the frame
+  if (sizechg.cxnew < sizechg.cxold || sizechg.cynew < sizechg.cyold)
+  { PPResizeConstr cstr;
+    if (WinQueryPresParam(hwnd, PPU_RESIZECONSTR, 0, NULL, sizeof cstr, &cstr, QPF_NOINHERIT) != 0)
+    { if (sizechg.cxnew < cstr.cx_min && sizechg.cxnew < sizechg.cxold)
+        sizechg.cxnew = cstr.cx_min;
+      if (sizechg.cynew < cstr.cy_min && sizechg.cynew < sizechg.cyold)
+        sizechg.cynew = cstr.cy_min;
+      // A size constraint at the top level is assumed to be sufficient.
+      goto done;
+    }
+  }
+
+  // for all children
+  { HENUM en = WinBeginEnumWindows(hwnd);
+    PMASSERT(en != NULLHANDLE);
+    HWND child;
+    while ((child = WinGetNextWindow(en)) != NULLHANDLE)
+    { // find PPU_RESIZEINFO
+      PPResizeParams rsinfo = {{0},{1,1}};
+      if (WinQueryPresParam(child, PPU_RESIZEINFO, 0, NULL, sizeof rsinfo.rs, &rsinfo.rs, QPF_NOINHERIT) == 0)
+        continue;
+      WinQueryPresParam(child, PPU_RESIZECONSTR, 0, NULL, sizeof rsinfo.cs, &rsinfo.cs, QPF_NOINHERIT);
+      SWP childpos;
+      PMRASSERT(WinQueryWindowPos(child, &childpos));
+      DEBUGLOG(("dlg_adjust_resize: {%i/%i,%i/%i, %i/%i,%i/%i, %i,%i} :> %p {%x, %i,%i, %i,%i}\n",
+        rsinfo.rs.x_resize.Numerator, rsinfo.rs.x_resize.Denominator, rsinfo.rs.y_resize.Numerator, rsinfo.rs.y_resize.Denominator,
+        rsinfo.rs.cx_resize.Numerator, rsinfo.rs.cx_resize.Denominator, rsinfo.rs.cy_resize.Numerator, rsinfo.rs.cy_resize.Denominator,
+        rsinfo.cs.cx_min, rsinfo.cs.cy_min,
+        child, childpos.fl, childpos.x, childpos.y, childpos.cx, childpos.cy));
+      apply_resize(childpos, rsinfo, sizechg);
+    }
+    PMRASSERT(WinEndEnumWindows(en));
+  }
+
+ done:
+  // Apply adjustments
+  DEBUGLOG(("dlg_adjust_resize: %i,%i -> %i,%i\n", pswp->cx, pswp->cy, sizechg.cxnew, sizechg.cynew));
+  pswp->cx = sizechg.cxnew;
+  pswp->cy = sizechg.cynew;
+}
+
+void dlg_do_resize(HWND hwnd, SWP* pswpnew, SWP* pswpold)
+{ DEBUGLOG(("dlg_do_resize(%p, {%x, %i,%i, %i,%i}, {%x, %i,%i, %i,%i})\n", hwnd,
+    pswpnew->fl, pswpnew->cx, pswpnew->cy, pswpnew->x, pswpnew->y,
+    pswpold->fl, pswpold->cx, pswpold->cy, pswpold->x, pswpold->y));
+  if (pswpold->cx == pswpnew->cx && pswpold->cy == pswpnew->cy)
+    return;
+  SWRS sizechg = { pswpold->cx, pswpold->cy, pswpnew->cx, pswpnew->cy };
+  // for all children
+  HENUM en = WinBeginEnumWindows(hwnd);
+  PMASSERT(en != NULLHANDLE);
+  HWND child;
+  // count the children with PPU_RESIZEINFO first.
+  size_t count;
+  while ((child = WinGetNextWindow(en)) != NULLHANDLE)
+  { ULONG found = 0;
+    WinQueryPresParam(child, PPU_RESIZEINFO, 0, &found, 0, NULL, QPF_NOINHERIT);
+    if (found)
+      ++count;
+  }
+  if (count == 0)
+  { PMRASSERT(WinEndEnumWindows(en));
+    return;
+  }
+  // Allocate target structure.
+  SWP* childpos_list = (SWP*)malloc(count * sizeof(SWP));
+  count = 0;
+  PPResizeParams rsinfo = {{0},{1,1}};
+  while ((child = WinGetNextWindow(en)) != NULLHANDLE)
+  { // find PPU_RESIZEINFO, but do not load size constraints.
+    if (WinQueryPresParam(child, PPU_RESIZEINFO, 0, NULL, sizeof rsinfo.rs, &rsinfo.rs, QPF_NOINHERIT) == 0)
+      continue;
+    SWP* childpos = childpos_list + count++;
+    PMRASSERT(WinQueryWindowPos(child, childpos));
+    DEBUGLOG(("dlg_do_resize: {%i/%i,%i/%i, %i/%i,%i/%i, } :> {%x, %i,%i, %i,%i}\n",
+      rsinfo.rs.x_resize.Numerator, rsinfo.rs.x_resize.Denominator, rsinfo.rs.y_resize.Numerator, rsinfo.rs.y_resize.Denominator,
+      rsinfo.rs.cx_resize.Numerator, rsinfo.rs.cx_resize.Denominator, rsinfo.rs.cy_resize.Numerator, rsinfo.rs.cy_resize.Denominator,
+      childpos->fl, childpos->x, childpos->y, childpos->cx, childpos->cy));
+    childpos->fl = SWP_NOADJUST; // Fix: entry fields otherwise grow unlimited. (Probably PM bug.)
+    apply_resize(*childpos, rsinfo, sizechg);
+    DEBUGLOG(("dlg_do_resize: -> {%x, %i,%i, %i,%i}\n",
+      childpos->fl, childpos->x, childpos->y, childpos->cx, childpos->cy));
+  }
+  PMRASSERT(WinEndEnumWindows(en));
+  // Resize children
+  if (count)
+    PMRASSERT(WinSetMultWindowPos(WinQueryAnchorBlock(hwnd), childpos_list, count));
+  free(childpos_list);
+}
+
+HWND dlg_addcontrol( HWND hwnd, PSZ cls, PSZ text, ULONG style,
+                     LONG x, LONG y, LONG cx, LONG cy, SHORT after,
+                     USHORT id, PVOID ctldata, PVOID presparams )
+{ POINTL pos[2] = {{ x, y }, { x + cx, y + cy }};
+  HWND behind;
+  if (!WinMapDlgPoints( hwnd, pos, 2, TRUE ))
+    return NULLHANDLE;
+  behind = after == NULLHANDLE ? HWND_BOTTOM : WinWindowFromID( hwnd, after );
+  return WinCreateWindow( hwnd, cls, text, style, pos[0].x, pos[0].y, pos[1].x-pos[0].x, pos[1].y-pos[0].y,
+                          hwnd, behind, id, ctldata, presparams);
+}
