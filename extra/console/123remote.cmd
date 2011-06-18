87026228c48e05905c2769e7d053b384cdd5f0b8
Marcel Müller <pm123@maazl.de>
2011-06-18 16:22:49 +0000

    improved UI

--- extra/console/123remote.cmd
+++ extra/console/123remote.cmd
@@ -1,14 +1,16 @@
 /**/
 pipe = '\pipe\pm123'
 
-IF STREAM(pipe, 'c', 'open') \= 'READY:' THEN
-  CALL Error 21, 'Cannot open pipe 'pipe
+rc = STREAM(pipe, 'c', 'open')
+IF \ABBREV(rc, 'READY') THEN
+  CALL Error 21, 'Cannot open pipe 'pipe': 'rc
 
 IF ARG(1) = '' THEN DO
   SAY "PM123 remote console. Interactive mode. Enter an empty line to quit."
 
   DO FOREVER
-    PULL command
+    CALL CHAROUT , '> '
+    PARSE PULL command
     IF command = '' THEN
       LEAVE
     CALL Remote(command)
@@ -21,11 +23,14 @@ EXIT
 
 Remote: PROCEDURE EXPOSE pipe
   CALL LINEOUT pipe, '*'ARG(1)
+  rc = STREAM(pipe, 's')
+  IF \ABBREV(rc, 'READY') THEN
+    CALL Error 27, 'I/O error while writing pipe 'pipe': 'rc
   DO FOREVER
     line = LINEIN(pipe)
-    SAY line
     IF line = '' THEN
       LEAVE
+    SAY line
     END
   RETURN
 
