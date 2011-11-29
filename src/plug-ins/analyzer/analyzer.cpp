d4557e27d8a065553d15f0ff93634dcdda5c2f98
Marcel Müller <pm123@maazl.de>
2011-11-29 21:16:51 +0000

    forward WM_CHAR

--- src/plug-ins/analyzer/analyzer.cpp
+++ src/plug-ins/analyzer/analyzer.cpp
@@ -826,6 +826,7 @@ plg_win_proc( HWND hwnd, ULONG msg, MPARAM mp1, MPARAM mp2 )
     case DM_DROP:
     case 0x041f:
     case 0x041e:
+    case WM_CHAR:
     case WM_CONTEXTMENU:
     case WM_BUTTON2MOTIONSTART:
     case WM_BUTTON1MOTIONSTART:
