d48c71fb72c50b1cb0406071342623787094a8ed
Marcel Müller <pm123@maazl.de>
2012-12-31 17:08:02 +0000

    Fixed handling of source files in subfolders

--- src/config/makedep.cmd
+++ src/config/makedep.cmd
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2010 Marcel Mueller
+ * Copyright 2007-2012 Marcel Mueller
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -58,11 +58,11 @@ IF ARG(1) = '' THEN DO
    SAY "         the includes are not found."
    SAY " -R      extended recursive mode"
    SAY "         Same as -r but also deal with include files in -I path"
-   SAY " -s      Strip .. from include file paths if possible"
-   SAY " -i      Strict include check with <> and """""
+   SAY " -v      Strict include check with ""..."""
+   SAY " -V      Strict include check with <...> and ""..."""
    SAY " -c      Check for recursions"
-   SAY " -v      verbose mode"
    SAY " -x      Unix like file expansion, e.g. *.cpp"
+   SAY " -s      search in sub directories for sourcefiles too"
    SAY " -q      Do not print warnings"
    SAY " --      no more options"
    EXIT 48
@@ -76,14 +76,14 @@ includes       = 0
 opt.recursive  = 0
 opt.Irecursive = 0
 opt.subdir     = ""
-opt.strippar   = 0
 opt.strict     = 0
+opt.strict2    = 0
 opt.checkrec   = 0
-opt.verbose    = 0
 opt.wildcard   = 0
 opt.cwd        = DIRECTORY()
 opt.orexx      = 0
 opt.opencmd    = 'OPEN'
+opt.debug      = 0
 
 IF SUBSTR(opt.cwd, LENGTH(opt.cwd), 1) \= '\' THEN
   opt.cwd = opt.cwd'\'
@@ -129,18 +129,18 @@ DO WHILE params \= ''
          opt.recursive = 1
          opt.Irecursive = 1
          END
-       WHEN SUBSTR(param, 2) = 'S' THEN
-         opt.subdir = "S"
        WHEN SUBSTR(param, 2) = 's' THEN
-         opt.strippar = 1
-       WHEN SUBSTR(param, 2) = '1' THEN
+         opt.subdir = "S"
+       WHEN SUBSTR(param, 2) = 'v' THEN
          opt.strict = 1
+       WHEN SUBSTR(param, 2) = 'V' THEN
+         opt.strict2 = 1
        WHEN SUBSTR(param, 2) = 'c' THEN
          opt.checkrec = 1
-       WHEN SUBSTR(param, 2) = 'v' THEN
-         opt.verbose = 1
        WHEN SUBSTR(param, 2) = 'x' THEN
          opt.wildcard = 1
+       WHEN SUBSTR(param, 2) = '_' THEN
+         opt.debug = 1
        OTHERWISE
          SAY "illegal option '"param"'"
          END
@@ -165,24 +165,29 @@ DO i = 1 TO file.0
    IF opt.wildcard & (VERIFY(file.i, "*?", "M") \= 0) THEN DO
       CALL SysFileTree file.i, "match", "OF"opt.subdir
       IF match.0 \= 0 THEN DO
-         dir = FILESPEC('D',file.i)FILESPEC('P',file.i)
-         absdir = DIRECTORY() 
+         dir = DirFromPath(file.i)
+         absdir = DIRECTORY()
          IF dir \= '' THEN DO
             CALL SysFileTree dir, "absdir", "OD"
-            IF absdir.0 \= 0 THEN
-               absdir = absdir.1
+            IF absdir.0 = 0 THEN
+               CALL Error 60, 'Failed to get absolute path of 'dir
+            absdir = absdir.1
             END
-         IF SUBSTR(absdir, LENGTH(absdir)) \= '\' THEN
-            absdir = absdir'\'
+         dirlen = LENGTH(absdir) - LENGTH(dir) + 2
+         IF RIGHT(absdir, 1) \= '\' THEN
+            dirlen = dirlen + 1
+         IF RIGHT(dir, 1) \= '\' THEN
+            dirlen = dirlen - 1
          DO j = 1 TO match.0
-            /*SAY dir"_"absdir"_"match.j*/
-            CALL DoFile dir||SUBSTR(match.j, LENGTH(absdir)+1)
+            /*SAY 'W: 'dir"_"absdir"_"match.j*/
+            CALL DoFile SUBSTR(match.j, dirlen)
             END
          ITERATE
          END
       END
-   /* single file mode */
-   CALL DoFile file.i
+   ELSE
+      /* single file mode */
+      CALL DoFile file.i
    END
 
 IF opt.checkrec THEN
@@ -217,7 +222,7 @@ WriteRules: PROCEDURE EXPOSE opt. rule.
          IF p = 0 THEN
             ITERATE
          /* check wether we have a rule */
-         symname = SymName(STRIP(SUBSTR(data, 1, p-1)))
+         symname = SymName(MakeAbs(STRIP(SUBSTR(data, 1, p-1)), opt.cwd))
          IF SYMBOL(symname) \= 'VAR' THEN
             ITERATE
          /* parse dependencies */
@@ -247,8 +252,11 @@ WriteRules: PROCEDURE EXPOSE opt. rule.
       END
    /* append (remaining) rules */
    DO i = 1 to rule.0
-      symname = Symname(rule.i)
+      symname = Symname(NormPath(MakeAbs(rule.i, opt.cwd)))
       dep = VALUE(symname)
+      /*SAY "W: "rule.i symname dep*/
+      IF ABBREV(dep, 'RULE') THEN
+         CALL Assert 'Inexistent rule 'rule.i
       /* ignore empty dependencies */
       IF dep = '' THEN
          ITERATE
@@ -265,26 +273,46 @@ WriteRules: PROCEDURE EXPOSE opt. rule.
  * ARG(1)  file
  */
 DoFile: PROCEDURE EXPOSE opt. rule.
+   file = TRANSLATE(ARG(1), '\', '/')
+   /* check source file */
+   filepath = STREAM(file, "C", "QUERY EXISTS")
+   IF filepath = '' THEN
+      CALL Assert file" does not exist"
    /* Do nothing if the file is already known */
-   symname = SymName(ARG(1))
-   /*SAY 'DoFile: 'ARG(1) SYMBOL(symname)*/
+   symname = SymName(filepath)
+   IF opt.debug THEN
+      SAY 'DoFile: 'file filepath SYMBOL(symname)
+   IF POS('\..\', file) THEN
+      CALL Assert 'Invalid file name 'file
+   /*IF ABBREV(file, '\') | ABBREV(SUBSTR(file, 2), ':\') THEN
+      CALL Assert 'DoFile resulted in absolute path 'file*/
    IF SYMBOL(symname) = 'VAR' THEN
       RETURN ''
    CALL VALUE symname, ''
    /* remember list of rules */
    i = rule.0 + 1
-   rule.i = ARG(1)
+   rule.i = NormPath(filepath, opt.cwd)
    rule.0 = i
-   /* check source file */
-   IF STREAM(ARG(1), "C", "QUERY EXISTS") = '' THEN
-      RETURN Warn(ARG(1)" does not exist")
-   IF \ABBREV(STREAM(ARG(1), "C", opt.opencmd), "READY") THEN
-      RETURN Warn("Cannot open "ARG(1))
+   /* change to file's directory */
+   lastdir = DIRECTORY()
+   dir = DirFromPath(filepath)
+   /*SAY "D:" dir file*/
+   IF dir = '' THEN
+      dir = DIRECTORY()
+   ELSE DO
+      dir = DIRECTORY(dir)
+      IF dir = '' THEN
+         CALL Error 60, 'Cannot enter directory of 'file', currently at 'DIRECTORY()
+      END
    /* Parse file */
+   IF \ABBREV(STREAM(filepath, "C", opt.opencmd), "READY") THEN DO
+      CALL DIRECTORY(lastdir)
+      RETURN Warn("Cannot open "filepath)
+      END
    line = 0
    DO FOREVER
-      src = STRIP(LINEIN(ARG(1)))
-      IF STREAM(ARG(1), "S") \= "READY" THEN
+      src = STRIP(LINEIN(filepath))
+      IF STREAM(filepath, "S") \= "READY" THEN
          LEAVE
       line = line + 1
       /* Search for \s*#\s*include */
@@ -295,57 +323,62 @@ DoFile: PROCEDURE EXPOSE opt. rule.
          ITERATE
       src = STRIP(SUBSTR(src, 8), 'T')
       /* catch filename */
-      PARSE VAR src '"'file'"'
-      IF file \= '' THEN DO
+      PARSE VAR src '"'inc'"'
+      IF inc \= '' THEN DO
          /* local include */
-         IF STREAM(file, "C", "QUERY EXISTS") = '' THEN DO
-            IF opt.strict THEN
-               CALL Warn ARG(1)" line "line": Include "file" does not exist"
-            ELSE
-               /* try non-local too */
-               CALL NonlocalInclude file, symname, line" of "ARG(1)
-            ITERATE
+         path = STREAM(inc, "C", "QUERY EXISTS")
+         /*SAY "L:" inc path file*/
+         IF path \= '' THEN DO
+            /* found local => add dependency */
+            path = NormPath(path, opt.cwd)
+            CALL DoInclude symname, path
+            /* recursive? */
+            IF opt.recursive THEN
+               CALL DoFile inc
             END
-         /* found => add dependency */
-         CALL DoInclude symname, file
-         /* recursive? */
-         IF opt.recursive THEN
-            CALL DoFile file
+         /* try non-local too */
+         ELSE IF \NonlocalInclude(file, symname) THEN DO
+            IF opt.strict THEN
+               CALL Warn "File "inc" included at line "line" of "file": does not exist."
+            END
          ITERATE
          END
-      PARSE VAR src '<'file'>'
-      IF file = '' THEN DO
-         CALL Warn ARG(1)" line "line": found invalid #include directive"
+      PARSE VAR src '<'inc'>'
+      IF inc = '' THEN DO
+         CALL Warn file" line "line": found invalid #include directive"
          ITERATE
          END
       /* nonlocal include */
-      CALL NonlocalInclude file, symname, line" of "ARG(1)
+      IF \NonlocalInclude(inc, symname) THEN DO
+         IF opt.strict2 THEN
+            IF opt.include.0 = 0 THEN
+               CALL Warn "File "inc" included at line "line" of "file": not found because of missing -I option."
+            ELSE
+               CALL Warn "File "inc" included at line "line" of "file": not found."
+         END
       END
    /* close file */
-   CALL STREAM ARG(1), "C", "CLOSE"
+   CALL STREAM filepath, "C", "CLOSE"
+   CALL DIRECTORY(lastdir)
    RETURN ''
 
 /* Search for non-local include
  * ARG(1) include file name
- * ARG(2) symbol name
- * ARG(3) context
+ * ARG(2) parent file's symbol name
+ * RETURN true if succeded
  */
 NonlocalInclude: PROCEDURE EXPOSE opt. rule.
    /* nonlocal include */
-   IF opt.include.0 = 0 THEN DO
-      IF opt.verbose THEN
-         CALL Warn "File "ARG(1)" included at line "ARG(3)": not found because of missing -I option."
-      RETURN
-      END
+   IF opt.include.0 = 0 THEN
+      RETURN 0
    /* search include directories */
    DO i = 1 TO opt.include.0
       filepath = opt.include.i||ARG(1)
       /* include found? */
-      IF STREAM(filepath, "C", "QUERY EXISTS") \= '' THEN DO
-         /* remove ../ */
-         IF opt.strippar THEN
-            filepath = NormPath(filepath)
+      path = STREAM(filepath, "C", "QUERY EXISTS")
+      IF path \= '' THEN DO
          /* found => add dependency */
+         filepath = NormPath(filepath)
          CALL DoInclude ARG(2), filepath
          /* recursive? */
          IF opt.Irecursive THEN
@@ -354,28 +387,41 @@ NonlocalInclude: PROCEDURE EXPOSE opt. rule.
          END
       filepath = ''
       END
-   IF opt.verbose & (filepath = '') THEN
-      CALL Warn "File "file" included at line "ARG(3)": not found."
+   IF filepath = '' THEN
+      RETURN 0
+   RETURN 1
+
+/* Handle include file
+ * ARG(1)  parent file's symbol name
+ * ARG(2)  include file
+ */
+DoInclude: PROCEDURE EXPOSE rule. opt.
+   inc = ARG(2)
+   IF opt.debug THEN
+      SAY "DoInclude: "ARG(1)", "inc
+   IF POS('\..\', inc) THEN
+      CALL Assert 'Invalid not normalized include dependency 'inc
+   IF LEFT(inc, 1) = '\' | ABBREV(SUBSTR(inc, 2), ':') THEN
+      CALL Assert 'Invalid absolute include dependency 'inc
+   /* rules lookup */
+   dep = VALUE(ARG(1))
+   IF ABBREV(dep, 'RULE') THEN
+      CALL Assert 'DoInclude called with invalid symbol' ARG(1)
+   /* Is dependency not redundant */
+   IF POS(inc, dep) = 0 THEN
+      /* add dependency */
+      CALL VALUE ARG(1), dep' 'inc
    RETURN
 
 /* Normalize path
  * ARG(1)  include file path
+ * ARG(2)  path context (optional)
  * RETURN  reduced include path
  */
 NormPath: PROCEDURE EXPOSE opt.
-   filename = ARG(1)
    /* First we have to normalize slashes to make things easy. */
-   DO FOREVER
-      p = POS('/', filename)
-      IF p = 0 THEN
-         LEAVE
-      filename = OVERLAY('\', filename, p)
-      END
-   /* absolute path? */
-   IF ABBREV(filename, '\') | ABBREV(SUBSTR(filename, 2), ':\') THEN
-      RETURN ARG(1)
+   path = TRANSLATE(ARG(1), '\', '/')
    /* reduce /xxx/../ */
-   path = opt.cwd||filename
    /*SAY 'S:' path*/
    DO FOREVER
       p = POS('\..\', path)
@@ -387,42 +433,44 @@ NormPath: PROCEDURE EXPOSE opt.
       path = SUBSTR(path, 1, q)||SUBSTR(path, p+4)
       /*SAY 'SR:' SUBSTR(path, 1, p-1) path*/
       END
+   IF ARG(2, 'o') THEN
+      RETURN path
+   /*IF ABBREV(path, '\') | ABBREV(SUBSTR(path, 2), ':\') THEN
+      RETURN path*/
    /* Chkeck how many leading .. to keep */
-   p = COMPARE(TRANSLATE(opt.cwd), TRANSLATE(path))
-   /*SAY 'SC:' SUBSTR(opt.cwd, 1, p) p LENGTH(opt.cwd) path*/
-   IF p <= LENGTH(opt.cwd) THEN DO
-      p = LASTPOS('\', opt.cwd, p-1) +1
+   p = COMPARE(TRANSLATE(ARG(2)), TRANSLATE(path))
+   /*SAY 'SC:' SUBSTR(ARG(2), 1, p) p LENGTH(ARG(2)) path*/
+   IF p <= LENGTH(ARG(2)) THEN DO
+      p = LASTPOS('\', ARG(2), p-1) +1
       /* count \ in different part of opt.cwd */
-      q = WORDS(TRANSLATE(SUBSTR(opt.cwd, p), ' \', '\ '))
+      q = WORDS(TRANSLATE(SUBSTR(ARG(2), p), ' \', '\ '))
       /*SAY 'S\:' TRANSLATE(SUBSTR(opt.cwd, p), ' \', '\ ') q*/
-      filename = COPIES('..\', q)||SUBSTR(path, p)
+      path = COPIES('..\', q)||SUBSTR(path, p)
       END
    ELSE
-      filename = SUBSTR(path, p)
+      path = SUBSTR(path, p)
    /* done */
-   /*SAY 'SX:' opt.cwd ARG(1) filename
+   /*SAY 'SX:' opt.cwd ARG(1) path
    SAY*/
-   RETURN filename
+   RETURN path
 
-/* Handle include file
- * ARG(1)  parent file's symbol name
- * ARG(2)  include file
+/* Make path absolute
+ * ARG(1)  file name
+ * ARG(2)  path context
+ * RETURN  absolute path
  */
-DoInclude: PROCEDURE EXPOSE opt. rule.
-   /*SAY "DoInclude: "ARG(1)", "ARG(2)*/
-   /* rules lookup */
-   dep = VALUE(ARG(1))
-   /* Is dependency not redundant */
-   IF POS(ARG(2), dep) = 0 THEN
-      /* add dependency */
-      CALL VALUE ARG(1), dep' 'ARG(2)
-   RETURN
+MakeAbs: PROCEDURE EXPOSE opt.
+   IF ABBREV(ARG(1), '\') | ABBREV(SUBSTR(ARG(1), 2), ':\') THEN
+      RETURN ARG(1)
+   IF RIGHT(ARG(2), 1) = '\' THEN
+      RETURN NormPath(ARG(2)ARG(1))
+   RETURN NormPath(ARG(2)'\'ARG(1))
 
 /* Check for recursive entries
  * ARG(1)  file name
  */
 CheckRec: PROCEDURE EXPOSE opt. rule. stack.
-   sym = SymName(ARG(1))
+   sym = SymName(MakeAbs(ARG(1), opt.cwd))
    IF SYMBOL(sym) \= 'VAR' THEN
       /* No infos for this file => skip */
       RETURN
@@ -458,9 +506,32 @@ CheckRec: PROCEDURE EXPOSE opt. rule. stack.
 /* Create a unique symbol that represents the filename
  * Well, this will not work if the file name is too long.
  * ARG(1)  filename
- * RETURN  symbol */
+ * RETURN  symbol
+ */
 SymName: PROCEDURE
-   RETURN "RULE."C2X(TRANSLATE(ARG(1),'\','/'))
+   IF \ABBREV(ARG(1), '\') & \ABBREV(SUBSTR(ARG(1), 2), ':\') THEN
+      CALL Assert 'SymName called with relative path 'ARG(1)
+   inc = TRANSLATE(TRANSLATE(ARG(1)), '__ucsdtmpha', "\/_:;.,-+#'")
+   p = 1
+   DO FOREVER
+      p = VERIFY(inc, 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_',, p)
+      IF p = 0 THEN
+         LEAVE
+      inc = SUBSTR(inc, 1, p-1)'x'C2X(SUBSTR(inc, p, 1))SUBSTR(inc, p+1)
+      p = p + 3
+      END
+   RETURN "RULE."inc
+
+/* Get directory of absolute or relative file name
+ * ARG(1) file name
+ * RETURN directoory of file name without trailing \ except for the root
+ */
+DirFromPath: PROCEDURE
+   dir = FILESPEC('D', ARG(1))FILESPEC('P', ARG(1))
+   IF ((LENGTH(dir) > 3) | (POS(':',dir) = 0)) & (POS(RIGHT(dir, 1), '\/') \= 0) THEN
+      dir = LEFT(dir, LENGTH(dir) - 1)
+   RETURN dir
+
 
 /****************************************************************************
    Error handling
@@ -474,3 +545,6 @@ Warn: PROCEDURE EXPOSE opt.
       CALL LINEOUT STDERR, ARG(1)
    RETURN ARG(1)
 
+Assert: PROCEDURE
+   CALL LINEOUT STDERR, ARG(1)
+   CALL Die!
