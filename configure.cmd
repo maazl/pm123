1146fb2baed71cfd9ae8b4261fea6155f05d6728
Marcel Müller <pm123@maazl.de>
2014-03-23 21:06:56 +0000

    Added gcc4

--- configure.cmd
+++ configure.cmd
@@ -5,27 +5,37 @@
      call SysLoadFuncs
   end
 
-  comps.1.info   = "GNU C++"
+  comps.1.info   = "GNU C++ 4.x"
   comps.1.cc     = "g++.exe"
-  comps.1.opts   = "gcc"
-  comps.1.sig    = ""
+  comps.1.args   = "--version"
+  comps.1.opts   = "gcc_4"
+  comps.1.sig    = "(GCC) 4."
 
-  comps.2.info   = "IBM VisualAge C++ 3.6"
-  comps.2.cc     = "icc.exe"
-  comps.2.opts   = "icc_36"
-  comps.2.sig    = "Version 3.6"
+  comps.2.info   = "GNU C++ 3.x"
+  comps.2.cc     = "g++.exe"
+  comps.2.args   = "--version"
+  comps.2.opts   = "gcc_3"
+  comps.2.sig    = "(GCC) 3."
 
-  comps.3.info   = "Open Watcom C++"
-  comps.3.cc     = "wcl386.exe"
-  comps.3.opts   = "wcc"
-  comps.3.sig    = "Open Watcom"
+  comps.3.info   = "IBM VisualAge C++ 3.6"
+  comps.3.cc     = "icc.exe"
+  comps.3.args   = ""
+  comps.3.opts   = "icc_36"
+  comps.3.sig    = "Version 3.6"
 
-  comps.4.info   = "IBM VisualAge C++ 3.0"
-  comps.4.cc     = "icc.exe"
-  comps.4.opts   = "icc_30"
-  comps.4.sig    = "Version 3"
+  comps.4.info   = "Open Watcom C++"
+  comps.4.cc     = "wcl386.exe"
+  comps.4.args   = ""
+  comps.4.opts   = "wcc"
+  comps.4.sig    = "Open Watcom"
 
-  comps.0        = 4
+  comps.5.info   = "IBM VisualAge C++ 3.0"
+  comps.5.cc     = "icc.exe"
+  comps.5.args   = ""
+  comps.5.opts   = "icc_30"
+  comps.5.sig    = "Version 3"
+
+  comps.0        = 5
   comps.selected = 0
 
   parse arg options
@@ -48,7 +58,7 @@
      if options == "--"comps.i.opts then do
         comps.selected = i
         call configure
-     end   
+     end
   end
 
   say "The compiler is not specified (try 'configure --help' for more options)."
@@ -58,7 +68,7 @@
      if SysSearchPath( "PATH", comps.i.cc ) \= "" then do
 
         if comps.i.sig \= "" then
-          '@'comps.i.cc '2>&1 1| find "'comps.i.sig'" > nul'
+          '@'comps.i.cc comps.i.args '2>&1 1| find "'comps.i.sig'" > nul'
         else
            rc = 0
 
@@ -84,3 +94,4 @@ configure: procedure expose comps. files.
 
   say "Done."
   exit 0
+
